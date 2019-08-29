e7c010c0b4baeff0b566303be091caabe8cf05e1
Samet İrkören <sametirkoren@gmail.com>
2019-08-29 13:28:12 +0300

    Update a movie with new info PUT

--- routes/movie.js
+++ routes/movie.js
@@ -26,6 +26,19 @@ router.get('/:movie_id' , (req,res,next)=>{
   });
 });
 
+router.put('/:movie_id' , (req,res,next)=>{
+  const promise = Movie.findByIdAndUpdate(req.params.movie_id , req.body);
+  promise.then((movie)=>{
+    if(!movie)
+      next({message : 'The movie was not found.'  , code: 99 });
+    res.json(movie);
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+
+
+
 router.post('/', (req, res, next) => {
   const { title , imdb_score , category , country , year} = req.body; 
   const movie = new Movie({
