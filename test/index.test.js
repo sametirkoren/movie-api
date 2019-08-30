e892804c02550d92d9cde90711f4fcef501eb85f
Samet İrkören <sametirkoren@gmail.com>
2019-08-30 14:28:09 +0300

    mocha script

---
+++ test/index.test.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+
+
+const server = require('../app');
+
+chai.use(chaiHttp);
+
+
+// Testin ne testi olduğunu yazıyoruz.
+describe('Node Server' , ()=>{
+    it('(GET /) anasayfayı döndürür' , (done)=>{ // -> unit testi yapmak için
+        chai.request(server)
+            .get('/')
+            .end((err,res)=>{
+                res.should.have.status(200);
+                done();  // ->  done() testi bitti herşey yolunda sıkıntı yok demek.
+            }) 
+    }); 
+})
