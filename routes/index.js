83e2b46291f4dcfdd5a75e4102d13b204ae74914
Samet İrkören <sametirkoren@gmail.com>
2019-08-30 13:47:19 +0300

    Creating tokens with jsonwebtoken

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 
 // Models 
 const User = require('../models/User');
@@ -26,8 +26,42 @@ router.post('/register', (req, res, next) => {
       res.json(err);
     })
   })
+});
 
-
+router.post('/authenticate' , (req,res)=>{
+  const {username , password} = req.body;
+  User.findOne({
+    username
+  } , (err,user)=>{
+    if(err)
+      throw err ;
+    if(!user){
+      res.json({
+        status : false,
+        message : 'Authentication failed , user not found.'
+      });
+    }else{
+      bcrypt.compare(password, user.password).then((result)=>{
+        if(!result){
+          res.json({
+            status : false , 
+            message : 'Authentication failed , wrong password.'
+          });
+        }else{
+          const payload = {
+            username
+          };
+          const token = jwt.sign(payload , req.app.get('api_secret_key') , {
+            expiresIn : 720 // 12 SAAT
+          });
+          res.json({
+            status : true , 
+            token
+          })
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
