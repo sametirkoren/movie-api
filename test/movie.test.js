13bc26babe3376dba952fd1936e6e1e650a762d2
Samet İrkören <sametirkoren@gmail.com>
2019-08-30 15:47:16 +0300

    api/movies/:movie_id  write Test [PUT]

--- test/movie.test.js
+++ test/movie.test.js
@@ -7,7 +7,7 @@ const server = require('../app');
 
 chai.use(chaiHttp);
 
-let token ; 
+let token , movieId ; 
 // Testin ne testi olduğunu yazıyoruz.
 describe('/api/movies tests' , ()=>{
    before((done)=>{ // -> testler başlamadan önceden bir işlem yapmamızı sağlar
@@ -57,6 +57,27 @@ describe('/api/movies tests' , ()=>{
                 res.body.should.have.property('country');
                 res.body.should.have.property('year');
                 res.body.should.have.property('imdb_score');
+                movieId = res.body._id;
+                done();
+            });
+       });
+   });
+
+   describe('/GET/:director_id movie' , ()=>{
+       it('it should GET a movie by the given id' , (done)=>{
+        chai.request(server)
+            .get('/api/movies/'+ movieId)
+            .set('x-access-token' , token)
+            .end((err,res)=>{
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('title');
+                res.body.should.have.property('director_id');
+                res.body.should.have.property('category');
+                res.body.should.have.property('country');
+                res.body.should.have.property('year');
+                res.body.should.have.property('imdb_score');
+                res.body.should.have.property('_id').eql(movieId);
                 done();
             });
        });
